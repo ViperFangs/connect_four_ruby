11206c3408f71abfe52b4fe2d183f9264a79496e
Aarya <amathrej@asu.edu>
2022-08-20 02:58:38 -0700

    Partial Implmentation of #place_move

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,12 @@
 class Board
+  attr_reader :board
+
   def initialize(rows: 6, columns: 7)
     @board = create_board(rows, columns)
+  end
+
+  def place_move(column_number)
+    
   end
 
   def display_board
@@ -17,7 +23,6 @@ class Board
   private
 
   def create_board(rows, columns)
-
+    Array.new(rows, Array.new(columns, '_'))
   end
-
 end
