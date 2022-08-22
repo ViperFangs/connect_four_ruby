39f17d54604adf28906a7143012a479d8873b100
Aarya <amathrej@asu.edu>
2022-08-22 01:13:05 -0700

    Write tests for #place_move

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -16,6 +16,8 @@ describe Board do
   end
 
   describe '#place_move' do
+    let(:taken_space) {'o'}
+
     it 'Places a move at the seven\'nt column' do
       board.place_move(7)
       expect(board).to have_attributes(board: 
@@ -24,21 +26,38 @@ describe Board do
         ["_", "_", "_", "_", "_", "_", "_"], 
         ["_", "_", "_", "_", "_", "_", "_"], 
         ["_", "_", "_", "_", "_", "_", "_"], 
-        ["_", "_", "_", "_", "_", "_", "x"]])
+        ["_", "_", "_", "_", "_", "_", "#{taken_space}"]])
     end
   end
 
   describe '#display_board' do
+    let(:taken_space) {'o'}
+
     it 'displays a new board' do
-      answer = <<~HEREDOC
-      ['_', '_', '_', '_', '_', '_', '_']
-      ['_', '_', '_', '_', '_', '_', '_']
-      ['_', '_', '_', '_', '_', '_', '_']
-      ['_', '_', '_', '_', '_', '_', '_']
-      ['_', '_', '_', '_', '_', '_', '_']
-      ['_', '_', '_', '_', '_', '_', '_']
-      HEREDOC
-      expect(board.display_board).to eq(answer)
+      answer = "[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n"
+      expect{ board.display_board }.to output(answer).to_stdout
+    end
+
+    context 'displays the board after placing a move' do
+      it 'When you place the move in the 7th column' do 
+        answer = "[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"#{taken_space}\"]\n"
+        expect{ board.place_move(7).display_board }.to output(answer).to_stdout
+      end
+
+      it 'When you place the move in the 6th column' do 
+        answer = "[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"#{taken_space}\", \"_\"]\n"
+        expect{ board.place_move(6).display_board }.to output(answer).to_stdout
+      end
+
+      it 'When you place the move in the 6th and 7th column' do 
+        answer = "[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"#{taken_space}\", \"#{taken_space}\"]\n"
+        expect{ board.place_move(6).place_move(7).display_board }.to output(answer).to_stdout
+      end
+
+      it 'When you place 2 moves in the 7th column' do 
+        answer = "[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"_\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"#{taken_space}\"]\n[\"_\", \"_\", \"_\", \"_\", \"_\", \"_\", \"#{taken_space}\"]\n"
+        expect{ board.place_move(7).place_move(7).display_board }.to output(answer).to_stdout
+      end
     end
   end
 end
